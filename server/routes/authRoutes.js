2b7ab231fb5d7e4103776ef49d94ef117ed32efd
Muhammad Muizz <muizzmughal46@gmail.com>
2026-07-24 23:13:27 +0500

    setup backend

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { changePassword, login, session } from "../controllers/authController.js";
+
+
+const authRouter = Router();
+
+authRouter.post("/login", login)
+authRouter.get("/session", session)
+authRouter.post("/change-password", changePassword)
+
+export default authRouter;
