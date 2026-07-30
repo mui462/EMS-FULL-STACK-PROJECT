b3a60b301524a815cf7114af037feb7ab7c2a2ac
Muhammad Muizz <muizzmughal46@gmail.com>
2026-07-30 22:24:48 +0500

    fix: redirect root route to login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
       <Toaster />
 
       <Routes>
-       
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginLanding />} />
 
 
