50ccccb6f2fe4857d5b8e970a83710f0f3ae3218
Muhammad Muizz <muizzmughal46@gmail.com>
2026-08-03 21:26:41 +0500

    making it an app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-
+import { registerSW } from "virtual:pwa-register";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import {AuthProvider} from "./Context/AuthContext.jsx"
 
+registerSW({
+  immediate: true,
+});
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <AuthProvider>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,54 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
+
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "icon-192.png",
+        "icon-512.png"
+      ],
+
+      manifest: {
+        name: "Employee Management System",
+        short_name: "EMS",
+        description: "Employee Management System",
+
+        theme_color: "#2563EB",
+        background_color: "#FFFFFF",
+
+        display: "standalone",
+        orientation: "portrait",
+        start_url: "/",
+
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
+          }
+        ]
+      }
+    })
   ]
-})
+});
